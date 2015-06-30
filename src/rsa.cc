71db4a63c600d43ceb46c8b858606243ed5849e2
Jamis <hoojamis@gmail.com>
2015-07-01 00:22:11 +0800

    update rsa, fix bug

--- src/rsa.cc
+++ src/rsa.cc
@@ -18,8 +18,8 @@
 
 
 constexpr size_t kBITS = 1024;
-constexpr size_t kWORDS = kBITS / sizeof(ttmath::uint) / 8;
-
+constexpr size_t kBITS_A_WORD = sizeof(ttmath::uint) * 8;
+constexpr size_t kWORDS = kBITS / kBITS_A_WORD;
 
 template<size_t num_length>
 ttmath::UInt<num_length> pow_mod(const ttmath::UInt<num_length>& n, 
@@ -43,18 +43,20 @@ ttmath::UInt<num_length> pow_mod(const ttmath::UInt<num_length>& n,
 }
 
 
-bool is_prime(const ttmath::UInt<kWORDS / 2>& n) {
-    constexpr size_t num_trials = 64;
+template<size_t num_length>
+bool is_prime(const ttmath::UInt<num_length>& n) {
+    // std::cout << "n == " << n << std::endl;
+    constexpr size_t num_trials = 10;
 
     if (n < 2) return false;
     if (n == 2) return true;
     
     if (n % 2 == 0) return false;
 
-    ttmath::UInt<kWORDS / 2> s = 0;
-    ttmath::UInt<kWORDS / 2> d = n - 1;
+    ttmath::UInt<num_length> s = 0;
+    ttmath::UInt<num_length> d = n - 1;
 
-    ttmath::UInt<kWORDS / 2> quotient, remainder;
+    ttmath::UInt<num_length> quotient, remainder;
     while (1) {
         quotient = d / 2;
         remainder = d % 2;
@@ -64,36 +66,31 @@ bool is_prime(const ttmath::UInt<kWORDS / 2>& n) {
         d = quotient;
     }
 
-    ttmath::UInt<kWORDS / 2> tmp = 2;
-    tmp.Pow(s);
-    assert(tmp * d == n - 1);
-
-    auto try_composite = [&n, &d, &s](const ttmath::UInt<kWORDS / 2>& a)->bool {
-        if (pow_mod(a, d, n) == 1) return false;
-        for (ttmath::UInt<kWORDS / 2> i = 0; i < s; ++i) {
-            ttmath::UInt<kWORDS / 2> tmp = 2;
-            tmp.Pow(i);
-            if (pow_mod(a, tmp * d, n) == n - 1) return false;
-        }
-        return true;
-    };
-
     for (size_t i = 0; i < num_trials; ++i) {
-        ttmath::UInt<kWORDS / 2> rnd = 0;
-        for (size_t j = 0; j < kBITS / 2; ++j)
+        ttmath::UInt<num_length> rnd = 0;
+        for (size_t j = 0; j < kBITS_A_WORD * num_length; ++j)
             rnd |= (rand() & 1) == 1, rnd <<= 1;
-        rnd %= n - 2;
-        rnd += 2;
+        rnd %= n - 3, rnd += 2;
 
-        if (try_composite(rnd)) return false;
+        auto x = pow_mod(rnd, d, n);
+
+        if (x == 1 || x == n - 1) continue;
+
+        if (s == 1) return false;
+        
+        for (ttmath::UInt<num_length> j = 0; j < s - 1; ++j) {
+            x = pow_mod(x, ttmath::UInt<num_length>(2), n);
+            if (x == n - 1) break;
+            if (j == s - 2) return false;
+        }
     }
-
     return true;
 }
 
 
-void generate_random_prime(ttmath::UInt<kWORDS / 2>& p, const uintmax_t e) {
-    for (size_t i = 0; i < kBITS / 2; ++i)
+template<size_t num_length>
+void generate_random_prime(ttmath::UInt<num_length>& p, const uintmax_t e) {
+    for (size_t i = 0; i < kBITS_A_WORD * num_length; ++i)
         p |= (rand() & 1) == 1, p <<= 1;
     
     p ^= 1;
@@ -103,13 +100,14 @@ void generate_random_prime(ttmath::UInt<kWORDS / 2>& p, const uintmax_t e) {
     while (p % e == 1 || !is_prime(p)) p += 2;
 }
 
-ttmath::UInt<kWORDS> extended_gcd(ttmath::UInt<kWORDS> a, ttmath::UInt<kWORDS> b) {
-    ttmath::Int<kWORDS + 1> s = 0, old_s = 1,
-                            t = 1, old_t = 0,
-                            r = b, old_r = a;
+template<size_t num_length>
+ttmath::UInt<num_length> extended_gcd(ttmath::UInt<num_length> a, ttmath::UInt<num_length> b) {
+    ttmath::Int<num_length + 1> s = 0, old_s = 1,
+                                t = 1, old_t = 0,
+                                r = b, old_r = a;
     while (r != 0) {
-        ttmath::Int<kWORDS + 1> quotient = old_r / r;
-        ttmath::Int<kWORDS + 1> tmp;
+        ttmath::Int<num_length + 1> quotient = old_r / r;
+        ttmath::Int<num_length + 1> tmp;
         
         tmp = r;
         r = old_r - quotient * r;
@@ -124,13 +122,18 @@ ttmath::UInt<kWORDS> extended_gcd(ttmath::UInt<kWORDS> a, ttmath::UInt<kWORDS> b
         old_t = tmp;
     }
 
+    if (old_s < 0) old_s += b;
     return old_s;
 }
 
 
 int main() {
+    for (int i = 0; i < 100; ++i) {
+    srand(i);
+
     using namespace std;
 
+    
     uintmax_t e = 0x10001;
     
     ttmath::UInt<kWORDS / 2> p, q;
@@ -145,21 +148,38 @@ int main() {
     ttmath::UInt<kWORDS> phi_n = p - 1;
     phi_n *= q - 1;
 
-    ttmath::UInt<kWORDS> d = extended_gcd(e, phi_n);
+    ttmath::UInt<kWORDS> d = extended_gcd<kWORDS>(e, phi_n);
 
-    cout << p << endl;
-    cout << q << endl;
-    cout << n << endl;
-    cout << d << endl;
+    ttmath::UInt<kWORDS> message = 0;
+    for (size_t i = 0; i < kBITS / sizeof(int) / 8 / 2; ++i) {
+        message |= rand();
+        message <<= 8 * sizeof(int);
+    }
 
-    cout << "------------------------\n";
-
-    ttmath::UInt<kWORDS> message = rand();
     ttmath::UInt<kWORDS> cipher = pow_mod<kWORDS>(message, e, n);
     ttmath::UInt<kWORDS> plain = pow_mod(cipher, d, n);
-    cout << message << endl << plain << endl;
-    cipher = pow_mod(message, d, n);
-    plain = pow_mod<kWORDS>(cipher, e, n);
-    cout << message << endl << plain << endl;
+    // cipher = pow_mod(message, d, n);
+    // plain = pow_mod<kWORDS>(cipher, e, n);
 
+    if (message != plain) {
+        cerr << "Error! " << endl;
+
+        cerr << "seed == " << i << endl;
+        cout << "p == " << p << endl;
+        cout << "q == " << q << endl;
+        cout << "n == " << n << endl;
+        cout << "e == " << e << endl;
+        cout << "d == " << d << endl;
+        cout << "phi(n) == " << phi_n << endl;
+        cout << "------------------------\n";
+
+        cout << "message == " << message << endl;
+        cout << "cipher == " << cipher << endl;
+        cout << "plain == " << plain << endl;
+
+        return 1;
+    } else {
+        cout << "seed == " << i << endl;
+    }
+    }
 }
